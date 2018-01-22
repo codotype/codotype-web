b26e675462bb948ac3c1c6ebace827071c4d656f
Alexander Schwartzberg <aeksco@gmail.com>
2018-01-22 01:04:25 -0500

    Added ProjectPreview comtainer

---
+++ src/containers/project_preview/components/layout.vue
@@ -0,0 +1,33 @@
+
+<template>
+  <div class="row h-100">
+    <div class="col-lg-12 h-100">
+      <div class="card card-body bg-dark text-light border-light h-100">
+
+        <div class="row mb-2">
+          <div class="col-lg-12">
+            <h2>{{ model.label }}</h2>
+            <p class="lead">Preview</p>
+          </div>
+        </div>
+
+        <div class="row mb-2">
+          <div class="col-lg-12">
+            <iframe class='w-100 h-100' src="http://localhost:8080/#/projects"></iframe>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<!-- // // // //  -->
+
+<script>
+export default {
+  props: ['model']
+}
+</script>
+
+
---
+++ src/containers/project_preview/index.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <div class="container-fluid h-100">
+    <LayoutView :model="model"/>
+  </div>
+</template>
+
+<!-- // // // //  -->
+
+<script>
+import LayoutView from './components/layout.vue'
+
+export default {
+  name: 'project_preview',
+  components: {
+    LayoutView
+  },
+  metaInfo: {
+    title: 'Projects - Preview'
+  },
+  props: ['id'],
+  mounted () {
+    return this.$store.commit('project/select', { _id: this.id })
+  },
+  computed: {
+    model () {
+      return this.$store.getters['project/current']
+    }
+  }
+}
+</script>
