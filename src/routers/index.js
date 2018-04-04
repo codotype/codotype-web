f18e790db6514cd5f3b094ee3a191e10cc760518
Alexander Schwartzberg <aeksco@gmail.com>
2018-04-04 17:51:59 -0400

    Removed auth and user routes

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,8 +3,6 @@ import Router from 'vue-router'
 
 // Module routes
 // import { MainHomeRoute, MainAboutRoute } from './main'
-import { AuthLoginRoute, AuthRegisterRoute } from './auth'
-import { UserListRoute, UserShowRoute } from './user'
 // import { SchemaListRoute, SchemaNewRoute, SchemaShowRoute, SchemaEditRoute, RecordNewRoute, RecordEditRoute, RecordShowRoute } from './schema'
 import { SchemaListRoute, SchemaNewRoute, SchemaShowRoute, SchemaEditRoute } from './schema'
 import { ProjectListRoute } from './project'
@@ -17,10 +15,6 @@ export default new Router({
   routes: [
     // MainHomeRoute,
     // MainAboutRoute,
-    AuthLoginRoute,
-    AuthRegisterRoute,
-    UserListRoute,
-    UserShowRoute,
     SchemaListRoute,
     SchemaNewRoute,
     SchemaShowRoute,
