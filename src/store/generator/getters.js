0bdaeb1c201b5f6f1bd2bba2a4102290254379df
Alexander Schwartzberg <aeksco@gmail.com>
2018-04-07 15:01:22 -0400

    Added Generator vuex store

---
+++ src/store/generator/getters.js
@@ -0,0 +1,11 @@
+import { DEPLOYMENT_OPTS, CLIENT_OPTS } from './constants'
+
+// Generator Module Getters
+export default {
+  deploymentOpts: state => {
+    return DEPLOYMENT_OPTS
+  },
+  clientOpts: state => {
+    return CLIENT_OPTS
+  }
+}
