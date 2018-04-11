1da32986e8bdc6a58d267703bcd33e55eb5bb470
Alexander Schwartzberg <aeksco@gmail.com>
2018-04-10 23:34:43 -0400

    Added EditorHeader to SchemaList

--- src/containers/schema_list/components/Header.vue
+++ src/containers/schema_list/components/Header.vue
@@ -1,44 +1,37 @@
 
 <template>
-  <div class="row">
-    <div class="col-lg-6">
-      <h4>
-        Models
-      </h4>
-    </div>
-    <div class="col-lg-6">
+  <EditorHeader :help="'Define data modeled by the ' + project.label + 'app.'">
+    <div class="row">
+      <div class="col-lg-12">
 
-      <button class="btn btn-primary pull-right" v-b-modal="'new-schema'">
-        <i class="fa fa-fw fa-plus mr-2"></i>
-        New Schema
-      </button>
+        <button class="btn btn-primary pull-right" v-b-modal="'new-schema'">
+          <i class="fa fa-fw fa-plus mr-2"></i>
+          New Model
+        </button>
 
-      <!-- Bootstrap Modal Component -->
-      <!-- TODO - move this outside the scope of the loop, and instead pass only the options into a single instance -->
-      <b-modal id="new-schema"
-        :title="'New Schema'"
-        @ok="submit()"
-        header-bg-variant="dark"
-        header-text-variant="light"
-        body-bg-variant="dark"
-        body-text-variant="light"
-        footer-bg-variant="success"
-        footer-text-variant="light"
-        ok-variant='success'
-        ok-title='Create'
-        cancel-title='Cancel'
-        cancel-variant='dark'
-      >
-        <SchemaForm :schema="model" />
-        <!-- <pre class='text-left bg-light'>{{model}}</pre> -->
-      </b-modal>
+        <!-- Bootstrap Modal Component -->
+        <!-- TODO - move this outside the scope of the loop, and instead pass only the options into a single instance -->
+        <b-modal id="new-schema"
+          :title="'New Model'"
+          @ok="submit()"
+          header-bg-variant="dark"
+          header-text-variant="light"
+          body-bg-variant="dark"
+          body-text-variant="light"
+          footer-bg-variant="success"
+          footer-text-variant="light"
+          ok-variant='success'
+          ok-title='Create'
+          cancel-title='Cancel'
+          cancel-variant='dark'
+        >
+          <SchemaForm :schema="model" />
+          <!-- <pre class='text-left bg-light'>{{model}}</pre> -->
+        </b-modal>
 
+      </div>
     </div>
-    <div class="col-lg-12">
-      <small class="mb-2 form-text text-muted">Defines data modeled by the {{ project.label }} app.</small>
-      <hr>
-    </div>
-  </div>
+  </EditorHeader>
 </template>
 
 <script>
@@ -63,6 +56,5 @@ export default {
     submit: 'schema/create',
     resetNewModel: 'schema/resetNewModel'
   })
-
 }
 </script>
