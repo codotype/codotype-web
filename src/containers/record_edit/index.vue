efa4294315fbe2696b6ae5b20ea6048ec133487c
Alexander Schwartzberg <aeksco@gmail.com>
2018-04-10 23:40:46 -0400

    Added placeholder record edit container

---
+++ src/containers/record_edit/index.vue
@@ -0,0 +1,43 @@
+
+<template>
+  <div class="row">
+
+    <div class="col-lg-12">
+      <EditorHeader :help="'Defines the attributes that can be assigned to a single ' + schema.label" />
+    </div>
+
+    <!-- Record Form -->
+    <div class="col-lg-12">
+      <RecordForm :record="record" :onSubmit="createRecord" />
+    </div>
+
+  </div>
+</template>
+
+<!-- // // // //  -->
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import RecordForm from '@/components/record/RecordForm'
+
+export default {
+  props: ['project_id', 'schema_id'],
+  metaInfo: {
+    title: 'Seed - New'
+  },
+  components: {
+    RecordForm
+  },
+  created () {
+    this.resetNewRecord(this.schema_id)
+  },
+  computed: mapGetters({
+    record: 'record/newModel',
+    schema: 'schema/selectedModel'
+  }),
+  methods: mapActions({
+    createRecord: 'record/create',
+    resetNewRecord: 'record/resetNewModel'
+  })
+}
+</script>
