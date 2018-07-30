f3967da49d16dcff79373fafa401e281f086820a
Alexander Schwartzberg <aeksco@gmail.com>
2018-07-30 14:21:54 -0400

    Improved JSON export filename

--- src/store/project/actions.js
+++ src/store/project/actions.js
@@ -67,7 +67,7 @@ export default {
   },
 
   exportJson: ({ commit }, model) => {
-    DownloadFile(JSON.stringify(model, null, 2), 'blazeplate-export.json', 'application/json')
+    DownloadFile(JSON.stringify(model, null, 2), `${model.identifier}_blazeplate.json`, 'application/json')
   },
 
   setIdentifier: ({ state, commit }) => {
