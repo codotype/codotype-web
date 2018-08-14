d2a17d9453031946ab15f9dbd15d81302a550e85
Alexander Schwartzberg <aeksco@gmail.com>
2018-08-14 13:34:58 -0400

    Added AddonEditor, GlobalOptionsEditor, ModelOptionsEditor components

---
+++ src/containers/generator_new/components/ModelOptionsEditor.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="row">
+
+    <div class="col-lg-12">
+      <p class="lead mb-0">Model Options</p>
+      <small class="text-muted">Model Options expose an additional layer of configuration for individual generators.</small>
+      <MoreInfoLink url="https://github.com/codotype"/>
+      <hr>
+    </div>
+
+    <div class="col-lg-12">
+      <p class="card-text">Model Options</p>
+      <button class="btn btn-outline-primary">New Option</button>
+
+      <AddonAttributeList :collection="collection" />
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import MoreInfoLink from '@/components/MoreInfoLink'
+import AddonAttributeList from './AddonAttributeList'
+
+export default {
+  components: {
+    MoreInfoLink,
+    AddonAttributeList
+  },
+  computed: mapGetters({
+    collection: 'addon/collection'
+  })
+}
+</script>
--- src/containers/generator_new/index.vue
+++ src/containers/generator_new/index.vue
@@ -1,62 +1,48 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-lg-3">
+      <div class="col-lg-12">
 
-        <AddonList :collection="addonCollection" />
+        <b-tabs>
 
-        <br>
+          <b-tab title="Overview" active>
+            OVERVIEW
+            TODO - add OverviewEditor component
+          </b-tab>
 
-        <!-- New Addon Form -->
-        <button class="btn btn-primary w-100" v-b-modal="'new-addon'">
-          <i class="fa fa-fw fa-plus mr-2"></i>
-          New Addon
-        </button>
+          <b-tab title="Global Options">
+            <br>
+            <GlobalOptionsEditor />
+          </b-tab>
 
-        <b-modal id="new-addon" :title="'New Addon'" @ok="createAddon()" ok-title='Create' cancel-title='Cancel' >
-          <AddonForm :model="newAddon" />
-        </b-modal>
+          <b-tab title="Model Options" >
+            <br>
+            <ModelOptionsEditor />
+          </b-tab>
+
+          <b-tab title="Addons" active>
+            <br>
+            <AddonEditor/>
+          </b-tab>
+
+        </b-tabs>
 
       </div>
-
-        <!-- <FormInput label="Label" :required="true" placeholder="Label" v-model="model.label" example="Example: 'Last Name'" help="The human-readable name for this attribute." /> -->
-
-        <!-- <FormInput label="Label Plural" :required="true" placeholder="Label Plural" v-model="model.label_plural" example="Example: 'Last Name'" help="The human-readable name for this attribute." /> -->
-
-        <!-- <FormInput label="Description" :required="true" placeholder="Description" v-model="model.description" example="Example: 'Last Name'" help="The human-readable name for this attribute." /> -->
-
-        <!-- <FormInput label="More Info URL" :required="true" placeholder="More Info URL" v-model="model.more_info_url" example="Example: 'Last Name'" help="The human-readable name for this attribute." /> -->
-
-      <div class="col-lg-9">
-        <AddonDetail :model="selectedAddon" />
-      </div>
-
     </div>
   </div>
 </template>
 
 <script>
-import FormInput from '@/components/FormInput'
-import AddonForm from './components/AddonForm'
-import AddonList from './components/AddonList'
-import AddonDetail from './components/AddonDetail'
-import { mapGetters, mapActions } from 'vuex'
+import AddonEditor from './components/AddonEditor'
+import ModelOptionsEditor from './components/ModelOptionsEditor'
+import GlobalOptionsEditor from './components/GlobalOptionsEditor'
 
 export default {
   name: 'GeneratorForm',
   components: {
-    FormInput,
-    AddonForm,
-    AddonList,
-    AddonDetail
-  },
-  computed: mapGetters({
-    newAddon: 'addon/newModel',
-    addonCollection: 'addon/collection',
-    selectedAddon: 'addon/selectedModel'
-  }),
-  methods: mapActions({
-    createAddon: 'addon/create'
-  })
+    AddonEditor,
+    ModelOptionsEditor,
+    GlobalOptionsEditor
+  }
 }
 </script>
