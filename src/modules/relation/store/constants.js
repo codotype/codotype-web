ade4ab28b155272fd47c1fe96efb40903291813e
Alexander Schwartzberg <aeksco@gmail.com>
2018-08-17 21:17:29 -0400

    Added relation module - not wired up

---
+++ src/modules/relation/store/constants.js
@@ -0,0 +1,12 @@
+
+// Attribute constants
+export const DEFAULT_ATTRIBUTE = {
+  order: 0,
+  label: '',
+  identifier: '',
+  help: '',
+  required: false,
+  unique: false,
+  datatype: '',
+  datatypeOptions: {}
+}
