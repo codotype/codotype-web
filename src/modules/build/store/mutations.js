36810a7f32e11e7286ff3296f744b39431de1079
Alexander Schwartzberg <aeksco@gmail.com>
2018-10-09 14:17:25 -0400

    Updated build vuex store to work with latest API iteration

--- src/modules/build/store/mutations.js
+++ src/modules/build/store/mutations.js
@@ -4,6 +4,9 @@ import { NEW_MODEL_MUTATIONS } from '@/store/lib/mixins'
 
 export default {
   ...NEW_MODEL_MUTATIONS,
+  fetching (state, isFetching) {
+    state.fetching = isFetching
+  },
   choosingGenerator (state, choosing) {
     state.choosingGenerator = choosing
   }
