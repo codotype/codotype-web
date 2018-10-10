91cdbb67927702e5a312fc294715858cd973d7e6
Alexander Schwartzberg <aeksco@gmail.com>
2018-10-10 09:05:31 -0400

    Updated HelpButton, tour data

--- src/components/HelpButton.vue
+++ src/components/HelpButton.vue
@@ -1,7 +1,11 @@
 <template>
-  <a class='mr-3' role="button" @click.stop="startTour()">
-    <i class="fa fa-fw fa-lg fa-question-circle" v-b-tooltip.hover.left :title='"Tutorial"'></i>
-  </a>
+  <!-- <a class='mr-3' role="button" @click.stop="startTour()"> -->
+    <!-- <i class="fa fa-fw fa-lg fa-question-circle" v-b-tooltip.hover.left :title='"Tutorial"'></i> -->
+  <!-- </a> -->
+  <b-button variant='outline-dark' :size="size" @click.stop="startTour()" v-b-tooltip.hover.left :title='"Click here to start tour"'>
+    <i class="fa fa-question-circle"></i>
+    Tutorial
+  </b-button>
 </template>
 
 <script>
@@ -9,7 +13,7 @@ import Driver from 'driver.js'
 
 export default {
   name: 'HelpButton',
-  props: ['tour'],
+  props: ['tour', 'size'],
   created () {
     this.driver = new Driver()
   },
