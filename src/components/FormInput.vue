72d7cc0f36048c98b2211395b399dab4e3144c18
Alexander Schwartzberg <aeksco@gmail.com>
2018-10-29 12:52:55 -0400

    Added Focus prop to FormInput component

--- src/components/FormInput.vue
+++ src/components/FormInput.vue
@@ -7,17 +7,55 @@
     <small class="form-text text-muted" v-if="example && help">{{example}}<br>{{help}}</small>
     <small class="form-text text-muted" v-else-if="help">{{help}}</small>
 
-    <input v-if="type === 'BOOL'" type="checkbox" ref="input_el" :checked="value" @change="updateModel()">
-    <input v-else-if="type === 'DATE'" type="date" ref="input_el" class="form-control" :placeholder="placeholder" :value="value" @input="updateModel()" >
-    <input v-else-if="type === 'TIME'" type="time" ref="input_el" class="form-control" :placeholder="placeholder" :value="value" @input="updateModel()" >
-    <input v-else type="text" ref="input_el" class="form-control" :placeholder="placeholder" :value="value" @input="updateModel()" >
+    <input
+      v-if="type === 'BOOL'"
+      type="checkbox"
+      ref="input_el"
+      :checked="value"
+      @change="updateModel()"
+    >
+
+    <input
+      v-else-if="type === 'DATE'"
+      type="date"
+      ref="input_el"
+      class="form-control"
+      :placeholder="placeholder"
+      :value="value"
+      @input="updateModel()"
+    >
+
+    <input
+      v-else-if="type === 'TIME'"
+      type="time"
+      ref="input_el"
+      class="form-control"
+      :placeholder="placeholder"
+      :value="value"
+      @input="updateModel()"
+    >
+
+    <input
+      v-else
+      type="text"
+      ref="input_el"
+      class="form-control"
+      :placeholder="placeholder"
+      :value="value"
+      @input="updateModel()"
+    >
 
   </div>
 </template>
 
 <script>
 export default {
-  props: ['required', 'label', 'type', 'example', 'help', 'ex', 'help', 'placeholder', 'value'],
+  props: ['required', 'focus', 'label', 'type', 'example', 'help', 'ex', 'help', 'placeholder', 'value'],
+  mounted () {
+    if (this.focus) {
+      this.$refs.input_el.focus()
+    }
+  },
   methods: {
     updateModel () {
       if (this.type === 'BOOL') {
