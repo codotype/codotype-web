93aea028c56faedfde4d15f77c05aca7a76e56c5
Alexander Schwartzberg <aeksco@gmail.com>
2018-11-01 11:27:30 -0400

    Removed hard-coded navbar background CSS classes

--- src/components/BuildMenu.vue
+++ src/components/BuildMenu.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-navbar toggleable="md" type="light" variant="light" fixed="top" class='bg-white'>
+  <b-navbar toggleable="md" type="light" variant="light" fixed="top">
     <b-navbar-brand to="/blueprints">
       <strong>codotype</strong>
     </b-navbar-brand>
