c3654d90a6e7d164051d0a610794d02e2c826358
Alexander Schwartzberg <aeksco@gmail.com>
2018-11-23 19:33:39 -0500

    Generator show page incorperates home-page markup

--- src/modules/generator/pages/show.vue
+++ src/modules/generator/pages/show.vue
@@ -2,18 +2,18 @@
   <b-row>
     <b-col lg=12>
 
-      <b-row>
-        <b-col lg=12 class='d-flex justify-content-between align-items-center'>
-          <h2>{{ model.label }} Generator</h2>
+      <!-- <b-row> -->
+        <!-- <b-col lg=12 class='d-flex justify-content-between align-items-center'> -->
+          <!-- <h2>{{ model.label }} Generator</h2> -->
 
-          <b-button variant="success" size="lg">
-            <i class="fa fa-cog fa-spin"></i>
-            Generate Code
-          </b-button>
-        </b-col>
-      </b-row>
+          <!-- <b-button variant="success" size="lg"> -->
+            <!-- <i class="fa fa-cog fa-spin"></i> -->
+            <!-- Generate Code -->
+          <!-- </b-button> -->
+        <!-- </b-col> -->
+      <!-- </b-row> -->
 
-      <hr>
+      <!-- <hr> -->
 
       <b-tabs>
 
@@ -22,8 +22,52 @@
           title="Start"
           active
           class='card-body bg-white border border-top-0'
-          v-html="compiledMarkdown"
-        />
+        >
+
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="row">
+                <div class="col-lg-12 text-center">
+                  <h1 class='display-3' style="letter-spacing: .25rem !important;">Codotype</h1>
+                </div>
+              </div>
+
+              <div class="row py-2">
+
+                <div class="col-lg-12 text-center">
+                  <p class="lead">visual code scaffolding for the modern web</p>
+                  <p>Prototype new web applications amazingly fast</p>
+                  <p>Define your models, attributes, and relations - Codotype does the rest</p>
+                </div>
+
+                <div class="col-lg-12 text-center d-flex align-items-center justify-content-center">
+                  <gh-btns-star slug="codotype/codotype" show-count></gh-btns-star>
+                </div>
+              </div>
+
+              <div class="row justify-content-center">
+                <div class="col-lg-6 text-center">
+                  <hr class='mt-2'>
+                </div>
+              </div>
+
+              <div class="row mt-2 mb-4 justify-content-center">
+                <div class="col-lg-4">
+                  <b-button
+                    size="lg"
+                    variant="primary"
+                    block
+                  >
+                    <i class="fas fa-drafting-compass faa-wrench animated mr-2"></i>
+                    Start Your Next Project
+                  </b-button>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+        </b-tab>
 
         <!-- <b-tab -->
           <!-- title="Global Options" -->
