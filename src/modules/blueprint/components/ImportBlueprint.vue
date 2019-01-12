387e54cd2b779ffaeef854727594cfde46c6991f
Alexander Schwartzberg <aeksco@gmail.com>
2019-01-11 20:29:12 -0500

    Refined ImportBlueprint UX

--- src/modules/blueprint/components/ImportBlueprint.vue
+++ src/modules/blueprint/components/ImportBlueprint.vue
@@ -9,16 +9,30 @@
     cancel-title='Cancel'
     :ok-disabled="okDisabled"
   >
-    <div class="form-group">
-      <label class='mb-0'>
-        Import
-      </label>
-      <small class="form-text text-muted mb-2">Import a Codotype blueprint JSON file</small>
-      <input
-        type="file"
-        @change="processFile($event)"
-      >
+    <p class="form-text text-muted mb-2">Import a Codotype Blueprint stored in a .JSON file</p>
+
+    <div class="input-group mb-3">
+      <div class="custom-file">
+        <input
+          type="file"
+          class="custom-file-input"
+          accept=".json"
+          id="inputGroupFile01"
+          @change="processFile($event)">
+        <label class="custom-file-label" for="inputGroupFile01">Choose Blueprint</label>
+      </div>
     </div>
+
+    <small class="text-success" v-if="uploaded && blueprintValid">
+      <i class="fa fa-check-circle"></i>
+      Imports the {{blueprintJson.label}} Blueprint with {{blueprintJson.schemas.length}} models
+    </small>
+
+    <small class="text-danger" v-if="uploaded && !blueprintValid">
+      <i class="fa fa-times-circle"></i>
+      Invalid blueprint format
+    </small>
+
   </b-modal>
 </template>
 
@@ -28,17 +42,25 @@ export default {
   data () {
     return {
       blueprintJson: {},
-      okDisabled: true
+      uploaded: false,
+      okDisabled: true,
+      blueprintValid: false
     }
   },
   methods: {
     processFile (event) {
       const file = event.target.files[0]
       const reader = new FileReader()
+      this.okDisabled = true
       reader.onload = (e) => {
+        this.uploaded = true
         this.blueprintJson = JSON.parse(e.target.result)
         this.blueprintJson._id = null
-        this.okDisabled = false
+        // TODO - validate parsed blueprint in Vuex store
+        if (this.blueprintJson.schemas && this.blueprintJson.label) {
+          this.okDisabled = false
+          this.blueprintValid = true
+        }
       }
       reader.readAsText(file)
     }
