6807a63afafb44dfb1469746f7bb349ec748625b
Alexander Schwartzberg <aeksco@gmail.com>
2019-01-14 16:47:24 -0500

    Simplified GeneratorShow page, in-progress

---
+++ src/modules/generator/components/GeneratorStart.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <img class='generator-icon' :src="model.icon"/>
+          <h2>{{model.label}}</h2>
+        </div>
+      </div>
+
+      <div class="row py-2">
+
+        <div class="col-lg-12 text-center">
+          <!-- <p class="lead">visual code scaffolding for the modern web</p> -->
+          <!-- <p class="lead"></p> -->
+          <p class="lead">{{model.description}}</p>
+          <!-- <p>Prototype new web applications amazingly fast</p> -->
+          <!-- <p>Define your models, attributes, and relations - Codotype does the rest</p> -->
+        </div>
+
+        <div class="col-lg-12 text-center d-flex align-items-center justify-content-center">
+          <gh-btns-star slug="codotype/codotype" show-count></gh-btns-star>
+        </div>
+      </div>
+
+      <div class="row justify-content-center">
+        <div class="col-lg-6 text-center">
+          <hr class='mt-2'>
+        </div>
+      </div>
+
+      <div class="row mt-2 mb-4 justify-content-center">
+        <div class="col-lg-4">
+          <b-button
+            size="lg"
+            variant="primary"
+            block
+            @click="incrementStep()"
+          >
+            <i class="fas fa-drafting-compass faa-wrench animated mr-2"></i>
+            Start Your Next Project
+          </b-button>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['model', 'incrementStep']
+}
+</script>
+
+<style type="text/css" scoped>
+  .generator-icon {
+    max-width: 4rem;
+  }
+</style>
