fecdbe42e26ff97893c05c2b6ca48168bef3175d
Alexander Schwartzberg <aeksco@gmail.com>
2019-01-17 17:56:32 -0500

    Streamlining datatype constants throughout client UI

--- src/modules/schema/store/constants.js
+++ src/modules/schema/store/constants.js
@@ -35,16 +35,18 @@ export const RELATION_TYPES = [
 // TODO - these should be abstracted into @codotype/util
 export const DATATYPE_TEXT = 'TEXT'
 export const DATATYPE_STRING = 'STRING'
+export const DATATYPE_STRING_SELECT = 'STRING_SELECT'
 export const DATATYPE_STRING_ARRAY = 'STRING_ARRAY'
 export const DATATYPE_NUMBER = 'NUMBER'
 export const DATATYPE_NUMBER_INTEGER = 'NUMBER_INTEGER'
 export const DATATYPE_NUMBER_FLOAT = 'NUMBER_FLOAT'
 export const DATATYPE_NUMBER_DOUBLE = 'NUMBER_DOUBLE'
-export const DATATYPE_BOOL = 'BOOL'
+export const DATATYPE_BOOLEAN = 'BOOLEAN'
+export const DATATYPE_BOOLEAN_GROUP = 'BOOLEAN_GROUP'
 export const DATATYPE_DATE = 'DATE'
 export const DATATYPE_TIME = 'TIME'
 export const DATATYPE_DATETIME = 'DATETIME'
-export const DATATYPE_JSON = 'DATATYPE_JSON'
+export const DATATYPE_JSON = 'JSON'
 
 // TODO - these should be abstracted into @codotype/util
 export const DATATYPES = [
@@ -56,11 +58,10 @@ export const DATATYPES = [
   // { value: DATATYPE_NUMBER_ARRAY, text: 'Number Array', icon: 'fa fa-hashtag' },
   { value: DATATYPE_STRING, text: 'String', icon: 'fa fa-quote-left' },
   { value: DATATYPE_STRING_ARRAY, text: 'String Array', icon: 'fa fa-quote-left' },
-  { value: DATATYPE_BOOL, text: 'Boolean', icon: 'far fa-check-square' },
+  { value: DATATYPE_BOOLEAN, text: 'Boolean', icon: 'far fa-check-square' },
   { value: DATATYPE_DATE, text: 'Date', icon: 'far fa-calendar' },
   { value: DATATYPE_TIME, text: 'Time', icon: 'far fa-clock' },
   { value: DATATYPE_DATETIME, text: 'DateTime', icon: 'far fa-calendar-alt' },
-  // { value: DATATYPE_COLOR, text: 'Color', icon: 'far fa-eyedropper' },
   { value: DATATYPE_JSON, text: 'JSON', icon: 'fa fa-code' }
   // { value: 'GEO', text: 'Geolocation', icon: 'fa-map' },
 ]
