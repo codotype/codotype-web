d0b98e06bba26b124bacd99f7b8296b1f9ecfc5e
Alexander Schwartzberg <aeksco@gmail.com>
2019-02-06 22:57:35 -0500

    Removed record module (out of scope, unmaintained)

--- src/modules/record/store/mutations.js
+++
@@ -1,9 +0,0 @@
-import { COLLECTION_MUTATIONS, SELECT_MODEL_MUTATIONS, NEW_MODEL_MUTATIONS, EDIT_MODEL_MUTATIONS } from '@codotype/ui/src/store/lib/mixins'
-
-// Record Module mutations
-export default {
-  ...COLLECTION_MUTATIONS,
-  ...SELECT_MODEL_MUTATIONS,
-  ...NEW_MODEL_MUTATIONS,
-  ...EDIT_MODEL_MUTATIONS
-}
