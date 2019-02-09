1de4b3919bbff1642c66ba04e2d40bdfed946285
Alexander Schwartzberg <aeksco@gmail.com>
2019-02-08 23:52:55 -0500

    ConfigureGenerator UX refinement

--- src/modules/generator/components/ConfigureGenerator.vue
+++ src/modules/generator/components/ConfigureGenerator.vue
@@ -1,37 +1,70 @@
 <template>
-  <b-tabs lazy class='w-100' v-model="currentStep" card>
+  <b-tabs lazy class='w-100' v-model="currentStep">
     <!-- Generator Option Groups -->
     <b-tab
       lazy
-      class='card-body bg-white border border-top-0 pt-0'
-      :title="group.label"
+      class='pt-0'
+      :title="group.label_plural || group.label"
       v-for="group in model.option_groups"
       :key="group.identifier"
     >
       <br>
 
-      <EditorHeader
-        :title="group.label"
-        help="Configure global options for this generator"
-        url="https://codotype.github.io"
-      />
+      <template v-if="group.type === 'OPTION_GROUP_TYPE_GLOBAL_OPTION'">
+        <b-row class='justify-content-center'>
+          <b-col lg=9>
+            <EditorHeader
+              :title="group.label"
+              :help="group.description"
+              url="https://codotype.github.io"
+            />
+            <hr>
+          </b-col>
+        </b-row>
+      </template>
 
-      <hr>
+      <!-- OPTION_GROUP_TYPE_GLOBAL_BOOLEAN_GROUP -->
+      <template v-else-if="group.type === 'OPTION_GROUP_TYPE_GLOBAL_BOOLEAN_GROUP'">
 
-      <!-- OPTION_GROUP_TYPE_BOOLEAN_GROUP -->
-      <template v-if="group.type === 'OPTION_GROUP_TYPE_BOOLEAN_GROUP'">
-        <div class="mt-2" v-for="attr in group.attributes">
-          <OptionFormItem :model="attr" v-model="configurationObject[attr.identifier]"/>
-        </div>
+        <b-row class='justify-content-center'>
+          <b-col lg=9 class='border-right'>
+            <EditorHeader
+              :brs="true"
+              :title="group.label"
+              :help="group.description"
+              url="https://codotype.github.io"
+            />
+            <hr>
+          </b-col>
+          <b-col lg=6>
+            <div class="mt-2">
+              <ul class="list-group">
+                <li class="list-group-item" v-for="attr in group.attributes" :key="attr.identifier">
+                  <OptionFormItem :model="attr" v-model="configurationObject[group.identifier][attr.identifier]"/>
+                </li>
+              </ul class="list-group">
+            </div>
 
-        <div class="card card-body text-center bg-transparent border-warning text-warning" v-if="!group.attributes[0]">
-          <p class="lead mb-0">No options exposed by this generator</p>
-        </div>
+            <div class="card card-body text-center bg-transparent border-warning text-warning" v-if="!group.attributes[0]">
+              <p class="lead mb-0">No options exposed by this generator</p>
+            </div>
+          </b-col>
+        </b-row>
+
+      </template>
+
+      <template v-else>
+        <EditorHeader
+          :title="group.label"
+          :help="group.description"
+          url="https://codotype.github.io"
+        />
+        <hr>
       </template>
 
       <template v-if="group.type === 'OPTION_GROUP_TYPE_MODEL_ADDON'">
         <b-row>
-          <b-col lg=3>
+          <b-col lg=3 class='border-right'>
             <ul class="list-group">
               <li
                 v-for="schema in schemas"
@@ -107,7 +140,7 @@
       <!-- TODO - ABSTRACT INTO SEPARATE COMPONENT -->
       <template v-if="group.type === 'OPTION_GROUP_TYPE_MODEL_OPTION'">
         <b-row>
-          <b-col lg=3>
+          <b-col lg=3 class='border-right'>
             <ul class="list-group">
               <li
                 v-for="schema in schemas"
@@ -135,6 +168,30 @@
                 :schema="selectedSchema"
                 :model="attr"
                 v-model="configurationObject[group.identifier_plural][selectedSchemaId][attr.identifier]"
+              />
+            </div>
+
+          </b-col>
+        </b-row>
+
+        <div class="card card-body text-center bg-transparent border-warning text-warning" v-if="!group.attributes[0]">
+          <p class="lead mb-0">No options exposed by this generator</p>
+        </div>
+      </template>
+
+      <!-- TODO - ABSTRACT INTO SEPARATE COMPONENT -->
+      <template v-if="group.type === 'OPTION_GROUP_TYPE_GLOBAL_OPTION'">
+        <b-row class='justify-content-center'>
+          <b-col lg=9>
+
+            <!-- Header - Description -->
+            <!-- <small class="text-muted">{{group.description}}</small> -->
+
+            <!-- Define new instance -->
+            <div class="card card-body mt-2" v-for="attr in group.attributes">
+              <OptionFormItem
+                :model="attr"
+                v-model="configurationObject[group.identifier][attr.identifier]"
               />
             </div>
 
@@ -207,6 +264,7 @@ export default {
     const generator = this.$store.getters['generator/collection'].find(g => g.id === this.id)
 
     // Produces the generator configurationObject
+    // TODO - must be moved into Vuex store!
     const configurationObject = buildConfiguration({ schemas, generator })
     const selectedSchemaId = schemas[0].identifier
 
