ad5b5a5ec2d4812b75109702e6e7c40ef79b9951
Alexander Schwartzberg <aeksco@gmail.com>
2019-02-08 19:55:53 -0500

    Integrated HelpButton into BuildStpesControls

--- src/store/mediator.js
+++ src/store/mediator.js
@@ -1,4 +1,6 @@
-// TODO - remove this file
+import Driver from 'driver.js'
+
+// TODO - clean up this cruft, likely not needed
 export default function configureModerator (store, router) {
   // listen to mutations
   store.subscribe(({ type, payload }, state) => {
@@ -20,6 +22,19 @@ export default function configureModerator (store, router) {
           }
         }
         break
+      case 'build/steps/current':
+        console.log('INCREMENT')
+        console.log(payload)
+        let DEV = true // just so I don't go insane
+        if (payload === 1 && !DEV) {
+          setTimeout(() => {
+            let driver = new Driver()
+            let tourSteps = store.getters['tour/appEditorSteps']
+            driver.defineSteps(tourSteps)
+            driver.start()
+          }, 300)
+          break
+        }
     }
   })
 
