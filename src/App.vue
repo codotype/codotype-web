d09d9793ba9edc77a64538dcc572fd795ef7005b
Alexander Schwartzberg <aeksco@gmail.com>
2019-04-21 14:30:11 -0400

    Removed home module, now managed by codotype-www

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script>
-import Navbar from '@codotype/ui/src/components/Navbar'
+import Navbar from '@codotype/ui/src/components/GeneratorMenu'
 import Overlay from '@codotype/ui/src/components/Overlay'
 import AppFooter from '@codotype/ui/src/components/Footer'
 import Notifications from '@codotype/ui/src/modules/notification/components/Notifications'
